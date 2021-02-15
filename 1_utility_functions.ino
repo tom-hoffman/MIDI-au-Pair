5e557850786ae0562a261d9c270d27920ee19c25
Tom Hoffman <tom.hoffman@gmail.com>
2021-02-15 10:30:43 -0500

    Moving utilities into separate file.
    Just using the simple tab method on Arduino IDE.

---
+++ 1_utility_functions.ino
@@ -0,0 +1,43 @@
+// utility functions
+
+void writeDisplay() {
+  for(int i=0; i<4; i++) {
+    alpha4.writeDigitAscii(i, display_buffer[i]);
+  }
+  alpha4.writeDisplay();
+}
+
+void displayByte(byte b) {
+  // displays exact value of a byte in hex, right justified.
+  char a[2];
+  itoa(b, a, 16); // converting 
+  if (b <= 15) {
+    display_buffer[2] = '0';
+    display_buffer[3] = a[0];
+  }
+  else {
+    display_buffer[2] = a[0];
+    display_buffer[3] = a[1];
+  }
+  writeDisplay();
+}
+
+void displayButtonStatus(struct ButtonStatus B, char side) {
+  char t[3];
+  int i;
+  strcpy(t, presets[B.preset].id);
+  if (side == 'l') {i = 0;}
+  else {i = 2;}
+  display_buffer[i] = t[0];
+  display_buffer[i+1] = t[1];
+  writeDisplay();
+}
+
+int incrementPreset(byte current_preset) {
+  if (current_preset < (preset_count - 1)) {
+    return (current_preset + 1);
+  }
+  else {
+    return 0;
+  }
+}
--- midi_au_pair.ino
+++ midi_au_pair.ino
@@ -29,16 +29,20 @@ typedef struct Patch{
   byte toe_up;
 } Patch;
 
+// add ON/OFF
 typedef struct Preset{
-  char id[3];
-  struct Patch patches[4];
+  char id[3];               // two character string
+  struct Patch patches[4];  // 4 patches should be enough for anyone
+  byte on[2];               // controller/value to fire on start
+  byte off[2];              // controller/value to fire on end
 } Preset;
 
+// remember to set this to the total number of presets 
 const byte preset_count = 3;
 Preset presets[preset_count] = {
-  {"--", {0, 0, 0}},
-  {"T1", {1, 1, 1}},
-  {"T2", {2, 2, 2}}
+  {"--", {}, {0, 0}, {0, 0}},
+  {"T1", {}, {0, 0}, {0, 0}},
+  {"T2", {}, {0, 0}, {0, 0}}, 
 };
 
 typedef struct ButtonStatus{
@@ -53,14 +57,6 @@ ButtonStatus right_button = {2, 2, 6};
 // hardware setup boilerplate
 MIDI_CREATE_DEFAULT_INSTANCE();
 Adafruit_AlphaNum4 alpha4 = Adafruit_AlphaNum4();
-
-// Code below from SparkFun example code (MIT Licensed).
-static const byte PIN_BTN0 = 2;
-static const byte PIN_BTN1 = 3;
-static const byte PIN_BTN2 = 4;
-
-static const byte PIN_POT0 = 0;
-static const byte PIN_POT1 = 1;
 
 static const byte PIN_LED_GRN = 6;
 static const byte PIN_LED_RED = 7;
@@ -77,49 +73,7 @@ void initHardware() {
   digitalWrite(PIN_LED_RED, HIGH);
 }
 
-// utility functions
 
-void writeDisplay() {
-  for(int i=0; i<4; i++) {
-    alpha4.writeDigitAscii(i, display_buffer[i]);
-  }
-  alpha4.writeDisplay();
-}
-
-void displayByte(byte b) {
-  // displays exact value of a byte in hex, right justified.
-  char a[2];
-  itoa(b, a, 16); // converting 
-  if (b <= 15) {
-    display_buffer[2] = '0';
-    display_buffer[3] = a[0];
-  }
-  else {
-    display_buffer[2] = a[0];
-    display_buffer[3] = a[1];
-  }
-  writeDisplay();
-}
-
-void displayButtonStatus(struct ButtonStatus B, char side) {
-  char t[3];
-  int i;
-  strcpy(t, presets[B.preset].id);
-  if (side == 'l') {i = 0;}
-  else {i = 2;}
-  display_buffer[i] = t[0];
-  display_buffer[i+1] = t[1];
-  writeDisplay();
-}
-
-int incrementPreset(byte current_preset) {
-  if (current_preset < (preset_count - 1)) {
-    return (current_preset + 1);
-  }
-  else {
-    return 0;
-  }
-}
 
 // arduino sketch loops
 
